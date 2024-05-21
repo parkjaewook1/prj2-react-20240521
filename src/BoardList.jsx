496b515ce886943bc3f37e2b284a7f0eeb7143a1
jaewook <wodnr5050@gmail.com>
2024-05-21 14:39:33 +0900

    게시물 목록 테이블의 행 클릭시 게시물 보기로 이동

--- src/BoardList.jsx
+++ src/BoardList.jsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+  const navigate = useNavigate();
 
   // [{id:5, title:"제목1",writer:"누구1"},
   //  {id:5, title:"제목1",writer:"누구1"},
@@ -29,7 +31,14 @@ export function BoardList() {
           </Thead>
           <Tbody>
             {boardList.map((board) => (
-              <Tr key={board.id}>
+              <Tr
+                _hover={{
+                  bgColor: "gray.200",
+                }}
+                cursor={"pointer"}
+                onClick={() => navigate(`/board/${board.id}`)}
+                key={board.id}
+              >
                 <Td>{board.id}</Td>
                 <Td>{board.title}</Td>
                 <Td>{board.writer}</Td>
