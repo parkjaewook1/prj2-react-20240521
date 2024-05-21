336bcfa2349a4e675196e48f59327d1660a404e9
jaewook <wodnr5050@gmail.com>
2024-05-21 14:29:43 +0900

    게시물 목록 보기 화면

---
+++ src/BoardList.jsx
@@ -0,0 +1,43 @@
+import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPen } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+
+export function BoardList() {
+  const [boardList, setBoardList] = useState([]);
+
+  // [{id:5, title:"제목1",writer:"누구1"},
+  //  {id:5, title:"제목1",writer:"누구1"},
+  // {id:5, title:"제목1",writer:"누구1"}]
+
+  useEffect(() => {
+    axios.get("/api/board/list").then((res) => setBoardList(res.data));
+  }, []);
+
+  return (
+    <Box>
+      <Box>게시물 목록</Box>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>#</Th>
+              <Th>TITLE</Th>
+              <FontAwesomeIcon icon={faUserPen} />
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList.map((board) => (
+              <Tr key={board.id}>
+                <Td>{board.id}</Td>
+                <Td>{board.title}</Td>
+                <Td>{board.writer}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
+}
