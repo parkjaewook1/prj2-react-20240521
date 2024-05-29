64d799345a81a3fc87d4b5797d0e4d50977321af
jaewook <wodnr5050@gmail.com>
2024-05-29 15:34:10 +0900

    좋아요 클릭 후 like 객체 업데이트

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -86,17 +86,23 @@ export function BoardView() {
     return <Spinner />;
   }
 
+  function handleClickLike() {
+    axios
+      .put(`/api/board/like`, { boardId: board.id })
+      .then((res) => {
+        setLike(res.data);
+      })
+      .catch(() => {})
+      .finally(() => {});
+  }
+
   return (
     <Box>
       <flex>
         <Heading>{board.id}번 게시물</Heading>
         <Spacer />
         <Flex>
-          <Box
-            onClick={() => setLike({ ...like, like: !like.like })}
-            cursor="pointer"
-            fontSize="3xl"
-          >
+          <Box onClick={handleClickLike} cursor="pointer" fontSize="3xl">
             {like.like && <FontAwesomeIcon icon={fullThumb} />}
             {like.like || <FontAwesomeIcon icon={emptyThumb} />}
           </Box>
