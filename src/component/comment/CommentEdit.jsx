bef78e1e840b081397ec7fce3d60f42db8dfa097
jaewook <wodnr5050@gmail.com>
2024-05-31 12:12:19 +0900

    navBar css

--- src/component/comment/CommentEdit.jsx
+++ src/component/comment/CommentEdit.jsx
@@ -1,4 +1,17 @@
-import { Box, Button, Flex, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
@@ -12,16 +25,28 @@ export function CommentEdit({
   isProcessing,
 }) {
   const [commentText, setCommentText] = useState(comment.comment);
+  const { onClose, onOpen, isOpen } = useDisclosure();
+  const toast = useToast();
 
   function handleCommentSubmit() {
+    setIsProcessing(true);
     axios
       .put("/api/comment/edit", {
         id: comment.id,
         comment: commentText,
       })
-      .then(() => {})
+      .then(() => {
+        toast({
+          description: "댓글이 수정되었습니다.",
+          position: "top",
+          status: "success",
+        });
+      })
       .catch(() => {})
-      .finally(() => {});
+      .finally(() => {
+        setIsProcessing(false);
+        setIsEditing(false);
+      });
   }
 
   return (
@@ -41,13 +66,29 @@ export function CommentEdit({
           <FontAwesomeIcon icon={faXmark} />
         </Button>
         <Button
-          onClick={handleCommentSubmit}
+          isLoading={isProcessing}
+          onClick={onOpen}
           variant="outline"
           colorSchme={"blue"}
         >
           <FontAwesomeIcon icon={faPaperPlane} />
         </Button>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>수정 확인</ModalHeader>
+          <ModalBody>댓글을 저장하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button colorScheme={"gray"} onClick={onClose}>
+              취소
+            </Button>
+            <Button colorScheme={"blue"} onClick={handleCommentSubmit}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
